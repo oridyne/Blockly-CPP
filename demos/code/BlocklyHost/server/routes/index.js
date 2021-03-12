a9e8209701a4e5858cd7e2d0420b52cae57e110f
oridyne <nplasse@qmail.qcc.edu>
2021-03-12 12:18:54 -0500

    small changes

--- demos/code/BlocklyHost/server/routes/index.js
+++ demos/code/BlocklyHost/server/routes/index.js
@@ -1,10 +1,9 @@
-const e = require('cors');
 const 
     url = require('url'),
     chalk = require('chalk'),
     cppService = require('../service/cppService');
 
-async function requestHandler(req, res, wss) {
+async function requestHandler(req, res) {
     var reqURL = url.parse(req.url);
     var urlPath = reqURL.pathname.substring(1).split("/");
     var reqBody;
@@ -21,18 +20,19 @@ async function requestHandler(req, res, wss) {
         req.on('end', ()=> {
             if(reqRaw) {
                reqBody = JSON.parse(reqRaw.toString()); 
-               //console.log(reqRaw.toString());
             } 
             resolve(0);
         });
     });
 
+    //setting default response headers
     res.setHeader('Access-Control-Allow-Origin', "*");
     res.setHeader('Access-Control-Allow-Headers', "Content-Type");
     res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.setHeader("Content-Type", "text/plain");
     res.statusCode = 200;
 
+    //if req fails send back err
     if(await reqProm === 1) {
         res.writeHead(400);
         res.end();
@@ -51,6 +51,7 @@ async function requestHandler(req, res, wss) {
     }
 }
 
+// seperate to make stuff more readable
 function cppCompileRoute(urlPath, reqMethod, reqBody, res) {
     if(urlPath[1] === 'id' && reqMethod === 'GET' )  {
         cppService.getID(reqBody, res);
